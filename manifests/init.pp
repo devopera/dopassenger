0716ce9558c1b265090387585b27cd4815f4e9c2
cleverlight <github.account@cleveraspaint.com>
2014-02-27 20:53:38 +0000

    working on Ubuntu version

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,38 +13,95 @@ class dopassenger (
 ) {
 
   # install passenger deps
-  if ! defined(Package['gcc-c++']) {
-    package { 'gcc-c++' : ensure => 'installed', }
+  case $operatingsystem {
+    centos, redhat: {
+      if ! defined(Package['gcc-c++']) {
+        package { 'gcc-c++' : 
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['ruby-devel']) {
+        package { 'ruby-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['libcurl-devel']) {
+        package { 'libcurl-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['httpd-devel']) {
+        package { 'httpd-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['apr-devel']) {
+        package { 'apr-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['apr-util-devel']) {
+        package { 'apr-util-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['openssl-devel']) {
+        package { 'openssl-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['zlib-devel']) {
+        package { 'zlib-devel' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+    }
+    ubuntu, debian: {
+      if ! defined(Package['libcurl4-openssl-dev']) {
+        package { 'libcurl4-openssl-dev' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['apache2-threaded-dev']) {
+        package { 'apache2-threaded-dev' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['libapr1-dev']) {
+        package { 'libapr1-dev' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+      if ! defined(Package['libaprutil1-dev']) {
+        package { 'libaprutil1-dev' :
+          ensure => 'installed',
+          before => [Package['passenger']],
+        }
+      }
+    }
   }
-  if ! defined(Package['ruby-devel']) {
-    package { 'ruby-devel' : ensure => 'installed', }
-  }
-  # package doesn't seem to exist
-  if ! defined(Package['libcurl-devel']) {
-    package { 'libcurl-devel' : ensure => 'installed', }
-  }
-  if ! defined(Package['httpd-devel']) {
-    package { 'httpd-devel' : ensure => 'installed', }
-  }
-  if ! defined(Package['apr-devel']) {
-    package { 'apr-devel' : ensure => 'installed', }
-  }
-  if ! defined(Package['apr-util-devel']) {
-    package { 'apr-util-devel' : ensure => 'installed', }
-  }
-
   # install passenger gem
   if ! defined(Package['passenger']) {
     package { 'passenger' :
       ensure => 'installed',
       provider => 'gem',
-      require => [Package['gcc-c++'], Package['ruby-devel'], Package['libcurl-devel'], Package['httpd-devel'], Package['apr-devel'], Package['apr-util-devel ']],
     }
   }
-  
+
   # install apache2 module
   exec { 'dopassenger-apache2-install-module' :
-    path => '/bin:/usr/bin:/sbin:/usr/sbin',
+    path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
     command => 'passenger-install-apache2-module --auto',
     require => [Package['passenger']],
   }->
@@ -53,7 +110,6 @@ class dopassenger (
   exec { 'dopassenger-apache2-symlink-latest' :
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
     command => "find ${passenger_gems_path}/ -name 'passenger-*' -exec ln -s {} ${passenger_gems_path}/latest-passenger \;",
-    require => [Package['passenger']],
   }
 
 }
